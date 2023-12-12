f0312d460e944fd9d29287a2c860e66cedffa4cc
Prithika <144022982+sangeethamoorthy2023@users.noreply.github.com>
2023-12-12 13:12:53 +0530

    s

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path');
-const { Sequelize, DataTypes } = require('sequelize');
-
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const { Sequelize, DataTypes } = require("sequelize");
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.set('port', process.env.PORT || 5001);
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+app.get('/hotels', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'hotels.html'));
+});
+
+app.get('/services', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'services.html'));
+});
+
+
+app.get('/blog', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'blog.html'));
+});
+
+app.get('/about', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'about.html'));
+});
+
+app.get('/contact', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'contact.html'));
+});
+
+app.listen(app.get("port"), function () {
+  console.log("Node app is running at localhost:" + app.get("port"));
+});
